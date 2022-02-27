8245ad7229eac62a1fd6132ecb19e321081afcf0
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-02-28 02:31:24 +0530

    Add files via upload

---
+++ output_04_01.py
@@ -0,0 +1,8 @@
+Enter the number of discs: 3
+move disc 1 from A to C
+move 2 disc from A to B
+move disc 1 from C to B
+move 3 disc from A to C
+move disc 1 from B to A
+move 2 disc from B to C
+move disc 1 from A to C
