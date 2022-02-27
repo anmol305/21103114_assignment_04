80d97662d1a51a4a5c754b1f1cc7d746b7f7795c
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-02-28 02:41:07 +0530

    Add files via upload

---
+++ output_04_03.py
@@ -0,0 +1,29 @@
+Question 3
+
+Enter the first number:18
+Enter the second number (it should not be zero):4
+quotient is 4 remainder is 2
+part a 
+
+its callable
+
+part b 
+
+both are non zero
+
+part c 
+
+filtered out values are (5, 6) 
+
+part d 
+
+set data type is {5, 6}
+
+part e 
+
+set is now immutable
+
+part f 
+
+max value of set is 6
+hash value is 6221592716431983538
