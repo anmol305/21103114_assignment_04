23efba9c7915bf13cd464f7d7da3cc5b455b2eb1
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-02-28 02:43:54 +0530

    Add files via upload

---
+++ output_04_04.py
@@ -0,0 +1,13 @@
+Name of student is Anmol gupta
+Roll no of Anmol gupta is 21103114
+
+Name of student is Aman Choudhary
+Roll no of Aman Choudhary is 21103118
+
+Name of student is Pratyaksh mahajan
+Roll no of Pratyaksh mahajan is 21103116
+
+Anmol gupta record has been deleted 
+Aman Choudhary record has been deleted 
+Pratyaksh mahajan record has been deleted 
+ 
