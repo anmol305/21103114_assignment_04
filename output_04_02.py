d0dbc0d6e26b8fc340ce491f91fd976e0e63d72d
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-02-28 02:37:45 +0530

    Add files via upload

---
+++ output_04_02.py
@@ -0,0 +1,28 @@
+Enter the number of rows: 7
+            1   
+
+          1   1   
+
+        1   2   1   
+
+      1   3   3   1   
+
+    1   4   6   4   1   
+
+  1   5   10   10   5   1   
+
+1   6   15   20   15   6   1   
+
+Question 2
+
+Using recurrsion. 
+
+Enter the number(natural number) of lines to print: 7
+
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+ 1 5 10 10 5 1 
+1 6 15 20 15 6 1 
